6efaebc63c4ca5b11c305b3cd9383255516e950a
alirionx <dquilitzsch@outlook.de>
2018-08-21 17:52:15 +0000

    3rd sync

--- pm-booking.php
+++ pm-booking.php
@@ -178,7 +178,8 @@
 	
 //-----------------------------------------------------------------------------
 
-
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
 
 //--------Book Appointment-----------------------------------------------------
 
@@ -219,6 +220,79 @@
 			");
 		}
 		
+		
+		//------------------------------
+			
+			
+			$html_table = '
+				
+				<table>
+					<tr>
+						<th>period_book_period</th>
+						<th>period_book_slot</th>
+						<th>period_book_authid</th>
+						<th>period_book_hid</th>
+						<th>period_book_hit</th>
+						<th>period_book_pud</th>
+						<th>period_book_put</th>
+					</tr>
+					<tr>
+						<td>'.$period_id.'</td>
+						<td>'.$nfs_id.'</td>
+						<td>'.$authid.'</td>
+						<td>'.$hid.'</td>
+						<td>'.$hit.'</td>
+						<td>'.$pud.'</td>
+						<td>'.$put.'</td>
+					</tr>
+				</table>
+			';
+			
+			
+			require 'PHPMailer/src/Exception.php';
+			require 'PHPMailer/src/PHPMailer.php';
+			require 'PHPMailer/src/SMTP.php';
+			
+			
+			$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+			try {
+				//Server settings
+				//$mail->SMTPDebug = 2;                                 // Enable verbose debug output
+				$mail->isSMTP();                                      // Set mailer to use SMTP
+				$mail->Host = 'smtp.udag.de';  					      // Specify main and backup SMTP servers
+				$mail->SMTPAuth = true;                               // Enable SMTP authentication
+				$mail->Username = 'sp-cegeka-de-0001';                // SMTP username
+				$mail->Password = 'Oviss1234!';                       // SMTP password
+				$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+				$mail->Port = 587;                                    // TCP port to connect to
+
+				//Recipients
+				$mail->setFrom('info@sp-cegeka.de', 'AISS Info Services');
+				$mail->addAddress('dquilitzsch@outlook.de', 'Daniel Quilitzsch');     // Add a recipient
+				$mail->addAddress('daniel.quilitzsch@cegeka.de', 'Daniel Quilitzsch');     // Add a recipient
+				//$mail->addReplyTo('info@example.com', 'Information');
+				//$mail->addCC('cc@example.com');
+				//$mail->addBCC('bcc@example.com');
+
+				//Attachments
+				//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+				//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+				//Content
+				$mail->isHTML(true);                                  // Set email format to HTML
+				$mail->Subject = 'AISS PMP Mailer Test';
+				$mail->Body    = '<b>palimpalim</b><br><i>here comes the mail</i><br>'.$html_table;
+				//$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+				$mail->send();
+				echo 'Message has been sent';
+			} 
+			catch (Exception $e) {
+				echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+			}
+			
+			
+		//------------------------------
 	}
 	
 	function booking_cancel($POST){
